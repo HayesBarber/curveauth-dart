38bd28e62c7a89583f6a9e55e6f7f213ab5d0040
Hayes Barber <barberhayes@gmail.com>
2025-07-22 20:11:01 -0400

    generate key pair

---
+++ lib/src/ecc_key_pair.dart
@@ -0,0 +1,16 @@
+import 'package:cryptography/cryptography.dart';
+
+class ECCKeyPair {
+  final EcKeyPair keyPair;
+  final EcPublicKey publicKey;
+
+  ECCKeyPair._(this.keyPair, this.publicKey);
+
+  /// Generates a new key pair using P-256 curve
+  static Future<ECCKeyPair> generate() async {
+    final algorithm = Ecdsa.p256(Sha256());
+    final keyPair = await algorithm.newKeyPair();
+    final publicKey = await keyPair.extractPublicKey();
+    return ECCKeyPair._(keyPair, publicKey);
+  }
+}
